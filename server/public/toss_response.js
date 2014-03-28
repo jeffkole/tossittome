50ca36a449f05bc7fd74a2d2f6ee654430125a3c
Jeff Kolesky <jeff@kolesky.com>
2014-03-28 12:21:05 -0700

    Encapsulates toss_response in a function

--- server/public/toss_response.js
+++ server/public/toss_response.js
@@ -1,4 +1,6 @@
-var title = document.title;
-if (title.substring(0, 13) == '(Tossing...) ') {
-  document.title = title.substring(13);
-}
+(function() {
+  var title = document.title;
+  if (title.substring(0, 13) == '(Tossing...) ') {
+    document.title = title.substring(13);
+  }
+})();
