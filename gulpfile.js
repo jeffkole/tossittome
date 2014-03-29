9de461ccd475f1ea53ec2f3388d747f3b3d7ecdc
Jeff Kolesky <jeff@kolesky.com>
2014-03-29 16:10:07 -0700

    Adds task to make tarball of server code

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@ var gulp    = require('gulp'),
     clean   = require('gulp-clean'),
     exec    = require('gulp-exec'),
     filter  = require('gulp-filter'),
+    gzip    = require('gulp-gzip'),
     map     = require('map-stream'),
+    rename  = require('gulp-rename'),
     replace = require('gulp-replace'),
+    tar     = require('gulp-tar'),
     Q       = require('q');
 
 var logFile = function() {
@@ -12,12 +15,12 @@ var logFile = function() {
     gutil.log(file.path);
     cb(null, file);
   });
-}
+};
 var resolve = function(deferred) {
   return map(function(file, cb) {
     deferred.resolve(file);
   });
-}
+};
 
 var hosts = {
   'dev'  : { 'hostAndPort': 'localhost:9999', 'host': 'localhost' },
@@ -47,4 +50,43 @@ gulp.task('pack-extensions', ['clean'], function() {
           .pipe(exec('./bin/pack_extension.sh <%= file.path %> ~/.ssh/tossittome.pem'));
       }).done();
   });
+});
+
+var copyServer = function(env) {
+  return function() {
+    var deferred = Q.defer();
+    gulp.src('server/**')
+        .pipe(gulp.dest('build/dist/server/' + env))
+        .pipe(resolve(deferred));
+    return deferred.promise;
+  };
+};
+var copyExtension = function(env) {
+  return function() {
+    var deferred = Q.defer();
+    gulp.src('build/dist/extension/' + env + '.crx')
+        .pipe(rename('extension.crx'))
+        .pipe(gulp.dest('build/dist/server/' + env + '/extension'))
+        .pipe(resolve(deferred));
+    return deferred.promise;
+  };
+};
+var tarServer = function(env) {
+  return function() {
+    var deferred = Q.defer();
+    gulp.src('**', { 'cwd': process.cwd() + '/build/dist/server/' + env })
+        .pipe(tar(env + '-server.tar'))
+        .pipe(gzip())
+        .pipe(gulp.dest('build/dist'))
+        .pipe(resolve(deferred));
+    return deferred.promise;
+  };
+};
+
+gulp.task('dist', function() {
+  var env = 'prod';
+  copyServer(env)()
+    .then(copyExtension(env))
+    .then(tarServer(env))
+    .done();
 });
