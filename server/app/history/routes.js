ef3e0469e236839a85e3cc9d6769c84986be27a5
Scott Forman <sjforman@gmail.com>
2014-04-30 12:59:09 -0400

    not quite showing tosses on history page

--- server/app/history/routes.js
+++ server/app/history/routes.js
@@ -5,52 +5,30 @@ var fs      = require('fs'),
     db      = require('toss/common/db'),
     userDao = require('toss/user/dao');
 
-function getHistory(request, response) {
-  renderHistory(request, response);
-}
-
 function renderHistory(request, response) {
-
-var pages = {
-  "tosses" : [
-        {
-          "title": "x",
-        },
-        {
-          "title": "y",
-        }
-      ]
-}
-
-console.log(pages);
-pages = JSON.stringify(pages);
-console.log(pages);
 
   var connection = db.getConnection();
 
   connection.query('SELECT title from pages', function(err, result, fields) {
   if(err) 
-  {
-    throw err;
-  }
-  else 
-  {
-    for (var i in result) 
     {
-      console.log(result[i]);
+      throw err;
     }
-  }
+  else 
+    {
+      console.log(result);
+      response.render('history', {
+      result: result 
+      });
+    }
   });
 
   db.closeConnection(connection);
 
-  response.render('history', {
-  tosses : pages 
-  });
 }
 
 function setup(app) {
-  app.get('/history', getHistory);
+  app.get('/history', renderHistory);
 }
 
 module.exports = setup;
