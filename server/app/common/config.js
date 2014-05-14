f21dcbd7694a357d675220e4aab3821f4a01ed8f
Jeff Kolesky <jeff@kolesky.com>
2014-05-14 15:09:38 -0700

    Adds config for beta site

--- server/app/common/config.js
+++ server/app/common/config.js
@@ -27,6 +27,7 @@ var config = {
 };
 
 function setup() {
+  var secrets = {};
   if ('production' === process.env.NODE_ENV) {
     config.host = 'tossitto.me';
     config.db.schema = 'tossittome_prod';
@@ -34,20 +35,27 @@ function setup() {
     config.errors.exposeStack = false;
     config.email.subaccount = 'TossItToMe';
 
-    production = require(process.env.HOME + '/.ssh/tossittome/production_secrets.json');
-    config = merge(config, production);
+    secrets = require(process.env.HOME + '/.ssh/tossittome/production_secrets.json');
+  }
+  else if ('beta' === process.env.NODE_ENV) {
+    config.port = 9998;
+    config.host = 'tossitto.me';
+    config.db.schema = 'tossittome_prod';
+    config.log.level = 'INFO';
+    config.errors.exposeStack = false;
+
+    secrets = require(process.env.HOME + '/.ssh/tossittome/beta_secrets.json');
   }
   else if ('test' === process.env.NODE_ENV) {
     config.db.schema = 'tossittome_test';
     config.log.level = 'ERROR';
 
-    test = require(process.env.HOME + '/.ssh/tossittome/test_secrets.json');
-    config = merge(config, test);
+    secrets = require(process.env.HOME + '/.ssh/tossittome/test_secrets.json');
   }
   else {
-    development = require(process.env.HOME + '/.ssh/tossittome/development_secrets.json');
-    config = merge(config, development);
+    secrets = require(process.env.HOME + '/.ssh/tossittome/development_secrets.json');
   }
+  config = merge(config, secrets);
 
   return config;
 }
