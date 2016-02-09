88361be2e2e76f3857cbc477d441d5a766e4de0b
Jeff Kolesky <jeff@kolesky.com>
2016-02-08 21:15:27 -0800

    Fixes super long title pages

--- test/server/app/page/test_dao.js
+++ test/server/app/page/test_dao.js
@@ -17,6 +17,34 @@ describe('PageDAO', function() {
         db.closeConnection(connection, done);
       });
     });
+
+    it('should insert a page with a really long title', function(done) {
+      var connection = db.getConnection();
+      // The title from the page that first broke me:
+      // https://www.reddit.com/r/IAmA/comments/40cmzb/i_am_90_years_old_an_officer_during_wwii_a/
+      var title = 'I Am 90 Years Old - An officer during WWII, a retired educator, and more engaged with society today than I\'ve ever been before. AMA! : IAmA';
+      var tosserId = 1;
+      var catcherId = 4;
+      pageDao.insertPage(connection, tosserId, catcherId,
+        'http://tossitto.me', title,
+        function (error, page) {
+          util.handle(error);
+          page.should.have.property('id');
+          page.id.should.be.a.Number;
+          var start = 1;
+          var limit = 1;
+          pageDao.fetchTossHistory(connection, tosserId, start, limit,
+            function (error, pages) {
+              util.handle(error);
+              pages.length.should.equal(1);
+              pages[0].id.should.equal(page.id);
+              pages[0].title.should.equal(title.substring(0, 128));
+              db.closeConnection(connection, done);
+            }
+          );
+        }
+      );
+    });
   });
 
   describe('#fetchNextPages', function() {
